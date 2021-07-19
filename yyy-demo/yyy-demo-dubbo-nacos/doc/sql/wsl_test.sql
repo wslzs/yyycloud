aacc5ceb6f41d9b7b701fa94bc20b8b75b4a7a19
王诗林 <399038956@qq.com>
2021-07-19 16:07:31 +0800

    demo sql

---
+++ yyy-demo/yyy-demo-dubbo-nacos/doc/sql/wsl_test.sql
@@ -0,0 +1,38 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50725
+ Source Host           : localhost:3306
+ Source Schema         : wsl
+
+ Target Server Type    : MySQL
+ Target Server Version : 50725
+ File Encoding         : 65001
+
+ Date: 19/07/2021 15:44:01
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for wsl_test
+-- ----------------------------
+DROP TABLE IF EXISTS `wsl_test`;
+CREATE TABLE `wsl_test`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `age` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of wsl_test
+-- ----------------------------
+INSERT INTO `wsl_test` VALUES (1, '张1', '18', '2021-06-16 17:40:33');
+INSERT INTO `wsl_test` VALUES (2, '张2', '19', '2021-06-16 17:40:46');
+
+SET FOREIGN_KEY_CHECKS = 1;
